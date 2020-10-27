ac1c01c52e722344afd4e6004c19bb85ed3c20f5
BackdoorIncorp <backdoorincorparation@gmail.com>
2020-10-27 20:33:15 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+void main() {
+  runApp(new MaterialApp(
+    home : new MyApplication()
+  ));
+}
+class MyApplication extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+
+
+
+
+}
+enum Answers{
+  YES,NO,MAYBE
+}
+
+class _State extends State<MyApplication> {
+
+  String value = "";
+
+  void setValue(String _value) => setState(() => _value = value);
+
+  Future askUser()async{
+    switch(
+    await showDialog(context: context, child: 
+        new SimpleDialog(
+          title: new Text("Close"),
+          elevation: 20,
+          backgroundColor: Colors.teal,
+          children: <Widget>[
+            new SimpleDialogOption(child: new Text("Yes"),onPressed: (){Navigator.pop(context,Answers.YES);},),
+            new SimpleDialogOption(child: new Text("No"),onPressed: (){Navigator.pop(context,Answers.NO);},),
+            new SimpleDialogOption(child: new Text("Maybe"),onPressed: (){Navigator.pop(context,Answers.MAYBE);},),
+          ],
+        )
+    )
+    ) {
+      case Answers.YES:
+        setValue('Yes');
+        break;
+      case Answers.NO:
+        setValue('No');
+        break;
+      case Answers.MAYBE:
+        setValue('Maybe');
+        break;
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Name here"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text("Hello World"),
+              new RaisedButton(onPressed: askUser,child: new Text("Click me"),)
+            ],
+          ),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
